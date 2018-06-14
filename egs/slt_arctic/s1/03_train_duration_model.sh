df229d2340591fe3931976910adbf4b4a3e8666e
Jennifer Williams <jw@Jennifers-MacBook-Air.local>
2018-06-14 11:12:01 +0100

    hard code some duration model stuff

--- egs/slt_arctic/s1/03_train_duration_model.sh
+++ egs/slt_arctic/s1/03_train_duration_model.sh
@@ -3,17 +3,17 @@
 global_config_file=conf/global_settings.cfg
 source $global_config_file
 
-if test "$#" -ne 1; then
-    echo "################################"
-    echo "Usage:"
-    echo "./03_train_duration_model.sh <path_to_duration_conf_file>"
-    echo ""
-    echo "Default path to duration conf file: conf/duration_${Voice}.conf"
-    echo "################################"
-    exit 1
-fi
+#if test "$#" -ne 1; then
+#    echo "################################"
+#    echo "Usage:"
+#    echo "./03_train_duration_model.sh <path_to_duration_conf_file>"
+#    echo ""
+#    echo "Default path to duration conf file: conf/duration_${Voice}.conf"
+#    echo "################################"
+#    exit 1
+#fi
 
-duration_conf_file=$1
+duration_conf_file=conf/duration_merlin-benchmark.conf
 
 ### Step 3: train duration model ###
 echo "Step 3:"
