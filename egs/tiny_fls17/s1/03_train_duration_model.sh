748b16dc4df06b75341da4eee9a9438ab7e0bf98
Jennifer Williams <s1738075@bruegel.inf.ed.ac.uk>
2018-07-05 14:22:05 +0100

    creating tiny little acoustic modeling set

---
+++ egs/tiny_fls17/s1/03_train_duration_model.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+
+#global_config_file=conf/global_settings.cfg
+#source $global_config_file
+#if test "$#" -ne 1; then
+#    echo "################################"
+#    echo "Usage:"
+#    echo "./03_train_duration_model.sh <path_to_duration_conf_file>"
+#    echo ""
+#    echo "Default path to duration conf file: conf/duration_${Voice}.conf"
+#    echo "################################"
+#    exit 1
+#fi
+
+#duration_conf_file=$1
+duration_conf_file=conf/duration_merlin-benchmark.conf
+MerlinDir=/jmain01/home/JAD003/sxr01/jxw04-sxr01/merlin
+
+
+### Step 3: train duration model ###
+echo "Step 3:"
+echo "training duration model..."
+./scripts/submit.sh ${MerlinDir}/src/run_merlin.py $duration_conf_file >> duration_log.txt 2>&1 
+
+
